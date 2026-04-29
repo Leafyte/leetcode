cc490c250a68e3a8579a86a0242641e95cc15cc1
Karthik M <karthikm12790@gmail.com>
2026-04-29 20:41:59 +0530

    Create Question23.java

---
+++ Question23.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Question23 {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        PriorityQueue<ListNode> pq =
+            new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.offer(node);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (!pq.isEmpty()) {
+
+            ListNode smallest = pq.poll();
+
+            tail.next = smallest;
+            tail = tail.next;
+
+            if (smallest.next != null) {
+                pq.offer(smallest.next);
+            }
+        }
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+
+        while (head != null) {
+            System.out.print(head.val);
+
+            if (head.next != null) {
+                System.out.print(" -> ");
+            }
+
+            head = head.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Question23 obj = new Question23();
+
+        ListNode l1 = new ListNode(1, new ListNode(4, new ListNode(5)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode l3 = new ListNode(2, new ListNode(6));
+
+        ListNode[] lists = {l1, l2, l3};
+
+        ListNode result = obj.mergeKLists(lists);
+
+        printList(result);
+    }
+}
