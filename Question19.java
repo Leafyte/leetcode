bed0899d871a86360bea41659bf4a64be104caf6
Karthik M <karthikm12790@gmail.com>
2026-04-30 19:30:44 +0530

    Create Question19.java

---
+++ Question19.java
@@ -0,0 +1,66 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Question19 {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+            if (head.next != null) System.out.print(" -> ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Question19 obj = new Question19();
+
+        ListNode head = new ListNode(1,
+                new ListNode(2,
+                new ListNode(3,
+                new ListNode(4,
+                new ListNode(5)))));
+
+        int n = 2;
+
+        ListNode result = obj.removeNthFromEnd(head, n);
+
+        printList(result);
+    }
+}
