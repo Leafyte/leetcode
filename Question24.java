bcbcca3f0dfa08fa31605032d01d106d7d46f786
Karthik M <karthikm12790@gmail.com>
2026-05-05 20:49:06 +0530

    Create Question24.java

---
+++ Question24.java
@@ -0,0 +1,64 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Question24 {
+
+    public ListNode swapPairs(ListNode head) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+
+        while (prev.next != null && prev.next.next != null) {
+
+            ListNode first = prev.next;
+            ListNode second = prev.next.next;
+
+            // swap
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            prev = first;
+        }
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+            if (head.next != null) System.out.print(" -> ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Question24 obj = new Question24();
+
+        ListNode head = new ListNode(1,
+                new ListNode(2,
+                new ListNode(3,
+                new ListNode(4))));
+
+        ListNode result = obj.swapPairs(head);
+
+        printList(result);
+    }
+}
