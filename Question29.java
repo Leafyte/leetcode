6a61ce7fb81f12ae65293854ed0ee10df3fb07a1
Karthik M <karthikm12790@gmail.com>
2026-05-07 20:52:58 +0530

    Implement integer division method in Question29

---
+++ Question29.java
@@ -0,0 +1,47 @@
+public class Question29 {
+
+    public int divide(int dividend, int divisor) {
+
+        // overflow case
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor);
+
+        int result = 0;
+
+        while (a >= b) {
+
+            long temp = b;
+            int multiple = 1;
+
+            while (a >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            a -= temp;
+            result += multiple;
+        }
+
+        if ((dividend < 0) ^ (divisor < 0)) {
+            result = -result;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Question29 obj = new Question29();
+
+        int dividend = 10;
+        int divisor = 3;
+
+        int result = obj.divide(dividend, divisor);
+
+        System.out.println("Quotient: " + result);
+    }
+}
