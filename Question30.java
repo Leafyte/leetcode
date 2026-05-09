e867646ebbb4db96367296d0e611af36f1d8e733
Karthik M <karthikm12790@gmail.com>
2026-05-09 20:12:29 +0530

    Create Question30.java

---
+++ Question30.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+public class Question30 {
+
+    public List<Integer> findSubstring(String s, String[] words) {
+
+        List<Integer> result = new ArrayList<>();
+
+        if (s == null || s.length() == 0 || words.length == 0)
+            return result;
+
+        int wordLen = words[0].length();
+        int wordCount = words.length;
+
+        Map<String, Integer> wordMap = new HashMap<>();
+
+        for (String word : words) {
+            wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+        }
+
+        for (int i = 0; i < wordLen; i++) {
+
+            int left = i;
+            int count = 0;
+
+            Map<String, Integer> windowMap = new HashMap<>();
+
+            for (int right = i; right + wordLen <= s.length(); right += wordLen) {
+
+                String word = s.substring(right, right + wordLen);
+
+                if (wordMap.containsKey(word)) {
+
+                    windowMap.put(word,
+                            windowMap.getOrDefault(word, 0) + 1);
+
+                    count++;
+
+                    while (windowMap.get(word) > wordMap.get(word)) {
+
+                        String leftWord =
+                                s.substring(left, left + wordLen);
+
+                        windowMap.put(leftWord,
+                                windowMap.get(leftWord) - 1);
+
+                        left += wordLen;
+                        count--;
+                    }
+
+                    if (count == wordCount) {
+                        result.add(left);
+                    }
+                }
+
+                else {
+                    windowMap.clear();
+                    count = 0;
+                    left = right + wordLen;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Question30 obj = new Question30();
+
+        String s = "barfoothefoobarman";
+        String[] words = {"foo", "bar"};
+
+        List<Integer> result = obj.findSubstring(s, words);
+
+        System.out.println(result);
+    }
+}
