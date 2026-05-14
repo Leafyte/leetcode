e26e7cedbe2847e3ca08b77363892891967d4f2b
Karthik M <karthikm12790@gmail.com>
2026-05-14 18:51:20 +0530

    Create Question36.java

---
+++ Question36.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class Question36 {
+
+    public boolean isValidSudoku(char[][] board) {
+
+        HashSet<String> seen = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+
+            for (int j = 0; j < 9; j++) {
+
+                char num = board[i][j];
+
+                if (num != '.') {
+
+                    String row = num + " in row " + i;
+                    String col = num + " in col " + j;
+                    String box = num + " in box " + (i / 3) + "-" + (j / 3);
+
+                    // duplicate found
+                    if (!seen.add(row) ||
+                        !seen.add(col) ||
+                        !seen.add(box)) {
+
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Question36 obj = new Question36();
+
+        char[][] board = {
+            {'5','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        boolean result = obj.isValidSudoku(board);
+
+        System.out.println("Is Valid Sudoku: " + result);
+    }
+}
