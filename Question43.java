22a2a512efc3d504679158e229d11650731f37cc
Karthik M <karthikm12790@gmail.com>
2026-05-21 22:53:35 +0530

    Create Question43.java

---
+++ Question43.java
@@ -0,0 +1,56 @@
+public class Question43 {
+
+    public String multiply(String num1, String num2) {
+
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+
+        int m = num1.length();
+        int n = num2.length();
+
+        int[] result = new int[m + n];
+
+        for (int i = m - 1; i >= 0; i--) {
+
+            for (int j = n - 1; j >= 0; j--) {
+
+                int mul =
+                        (num1.charAt(i) - '0') *
+                        (num2.charAt(j) - '0');
+
+                int p1 = i + j;
+                int p2 = i + j + 1;
+
+                int sum = mul + result[p2];
+
+                result[p2] = sum % 10;
+                result[p1] += sum / 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int num : result) {
+
+            // skip leading zeros
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+
+        Question43 obj = new Question43();
+
+        String num1 = "123";
+        String num2 = "456";
+
+        String result = obj.multiply(num1, num2);
+
+        System.out.println("Product: " + result);
+    }
+}
