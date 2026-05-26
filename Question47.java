9b48cecd1f3694a0b1a56b20de9ad7e5818f9cde
Karthik M <karthikm12790@gmail.com>
2026-05-26 22:31:38 +0530

    Implement unique permutations generator in Question47

---
+++ Question47.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class Question47 {
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        backtrack(nums,
+                new boolean[nums.length],
+                new ArrayList<>(),
+                result);
+
+        return result;
+    }
+
+    private void backtrack(int[] nums,
+                           boolean[] used,
+                           List<Integer> current,
+                           List<List<Integer>> result) {
+
+        if (current.size() == nums.length) {
+
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (used[i]) {
+                continue;
+            }
+
+            if (i > 0 &&
+                nums[i] == nums[i - 1] &&
+                !used[i - 1]) {
+
+                continue;
+            }
+
+            used[i] = true;
+            current.add(nums[i]);
+
+            backtrack(nums, used, current, result);
+
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Question47 obj = new Question47();
+
+        int[] nums = {1, 1, 2};
+
+        List<List<Integer>> result = obj.permuteUnique(nums);
+
+        System.out.println(result);
+    }
+}
