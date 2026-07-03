55b4ed7ac117f20f734d282867e9c0b1ca789e86
Karthik M <karthikm12790@gmail.com>
2026-07-03 20:48:42 +0530

    Add isSameTree method to compare two trees
    Implemented a method to check if two binary trees are identical.

---
+++ Question100.java
@@ -0,0 +1,57 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Question100 {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) &&
+               isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+
+        Question100 obj = new Question100();
+
+        // Tree 1
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
+
+        // Tree 2
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
+
+        boolean result = obj.isSameTree(p, q);
+
+        System.out.println("Are the trees the same? " + result);
+    }
+}
