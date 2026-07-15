73774f4457b11f0c32f32cf82017f710a4812da9
Karthik M <karthikm12790@gmail.com>
2026-07-15 22:39:56 +0530

    Add Question3658 class with GCD method

---
+++ Question3658.java
@@ -0,0 +1,17 @@
+public class Question3658 {
+
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+
+    public static void main(String[] args) {
+
+        Question3658 obj = new Question3658();
+
+        int n = 4;
+
+        int result = obj.gcdOfOddEvenSums(n);
+
+        System.out.println("GCD: " + result);
+    }
+}
