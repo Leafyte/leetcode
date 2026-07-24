ec811db96a71f4262c667533f5ca16f77f89209d
Karthik M <karthikm12790@gmail.com>
2026-07-24 22:16:47 +0530

    Add palindrome checking method in Question125
    Implement a method to check if a string is a palindrome, ignoring non-alphanumeric characters and case.

---
+++ Question125.java
@@ -0,0 +1,41 @@
+public class Question125 {
+
+    public boolean isPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) !=
+                Character.toLowerCase(s.charAt(right))) {
+
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Question125 obj = new Question125();
+
+        String s = "A man, a plan, a canal: Panama";
+
+        boolean result = obj.isPalindrome(s);
+
+        System.out.println("Is Palindrome: " + result);
+    }
+}
