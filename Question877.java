32d9cdd3da6ad56fee52a49e9984b3158a867f31
Karthik M <karthikm12790@gmail.com>
2026-08-03 20:52:54 +0530

    Implement stoneGame method in Question877 class

---
+++ Question877.java
@@ -0,0 +1,17 @@
+public class Question877 {
+
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Question877 obj = new Question877();
+
+        int[] piles = {5, 3, 4, 5};
+
+        boolean result = obj.stoneGame(piles);
+
+        System.out.println("Alice Wins: " + result);
+    }
+}
