fb03f0ada1e90f020a968e26cbb4e5609e2f5cff
Karthik M <karthikm12790@gmail.com>
2026-08-04 18:57:52 +0530

    Create Question3378.java

---
+++ Question3378.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Question3378 {
+
+    public List<Integer> findMissingElements(int[] nums) {
+
+        List<Integer> result = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+
+        int min = nums[0];
+        int max = nums[0];
+
+        for (int num : nums) {
+            set.add(num);
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        for (int i = min; i <= max; i++) {
+            if (!set.contains(i)) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Question3378 obj = new Question3378();
+
+        int[] nums = {1, 3, 5, 6};
+
+        List<Integer> result = obj.findMissingElements(nums);
+
+        System.out.println("Missing Elements: " + result);
+    }
+}
