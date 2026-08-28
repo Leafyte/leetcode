d41ee67c6fa604c75eeb50d1a86f9ccddc5f943b
Karthik M <karthikm12790@gmail.com>
2026-08-28 20:59:04 +0530

    Create Question231.java

---
+++ Question231.java
@@ -0,0 +1,18 @@
+public class Question231 {
+
+    public boolean isPowerOfTwo(int n) {
+
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+
+    public static void main(String[] args) {
+
+        Question231 obj = new Question231();
+
+        int n = 16;
+
+        boolean result = obj.isPowerOfTwo(n);
+
+        System.out.println("Is Power of Two: " + result);
+    }
+}
