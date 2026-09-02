313507ed8069af2ae258dbb0ae44038bc943a1b4
Karthik M <karthikm12790@gmail.com>
2026-09-02 21:35:02 +0530

    Create Question3875.java

---
+++ Question3875.java
@@ -0,0 +1,18 @@
+public class Question3875 {
+
+    public boolean constructUniformParityArray(int[] nums1) {
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Question3875 obj = new Question3875();
+
+        int[] nums1 = {1, 2, 3, 4};
+
+        boolean result = obj.constructUniformParityArray(nums1);
+
+        System.out.println("Can Construct Uniform Parity Array: " + result);
+    }
+}
