b517f484a1150c1dec3e1bc00b53a4013562bd6c
Karthik M <karthikm12790@gmail.com>
2026-09-06 21:40:04 +0530

    Implement containsNearbyDuplicate method

---
+++ Question219.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Question219 {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (map.containsKey(nums[i])) {
+
+                int previousIndex = map.get(nums[i]);
+
+                if (i - previousIndex <= k) {
+                    return true;
+                }
+            }
+
+            // Always store the latest index
+            map.put(nums[i], i);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Question219 obj = new Question219();
+
+        int[] nums = {1, 2, 3, 1};
+        int k = 3;
+
+        boolean result = obj.containsNearbyDuplicate(nums, k);
+
+        System.out.println("Contains Nearby Duplicate: " + result);
+    }
+}
